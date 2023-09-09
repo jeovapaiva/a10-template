7fe90c644f040d8bbcc99dd957f5a192e4a6f0c7
jeovapaiva <jeovappaiva@gmail.com>
2023-09-09 19:09:55 -0300

    a010 arrays

---
+++ script.js
@@ -0,0 +1,31 @@
+let arrayString = ["banana", "mamão", "amora"];
+let arrayNumber = [8, 2, 5];
+let arraySortido = ["uva", 12, true];
+let arrayNum = [20];
+let arrayVazio = [];
+
+console.log(arrayString.length);
+console.log(arrayString[2]);
+console.log(arrayString);
+
+console.log(arraySortido.includes(true));
+console.log(arraySortido.includes(12));
+console.log(arraySortido.includes(10));
+console.log(arraySortido.includes("uva"));
+
+let copiarArrayString = arrayString;
+console.log(copiarArrayString);
+
+copiarArrayString.push("uva");
+console.log(copiarArrayString);
+console.log(arrayString);
+
+//let copiarArrayString1 = arrayString.slice();
+//console.log(copiarArrayString1);
+
+//copiarArrayString.pop();
+//console.log(copiarArrayString)
+
+copiarArrayString.splice(2,2);
+console.log(copiarArrayString);
+
